27fae3b6b115eb5b3a5d77bd10ab5580db20c554
Adrian <adrianhenry2115@gmail.com>
2024-01-11 10:43:51 -0500

    search

--- components/search.tsx
+++ components/search.tsx
@@ -7,12 +7,11 @@ const Search = () => {
     return (
         <div className="flex w-full justify-center items-center">
             <Autocomplete
-                className="w-11/12 p-2 flex bg-zinc-800 text-white rounded-lg lg:w-1/2"
+                className="w-11/12 flex bg-zinc-600 text-white rounded-lg lg:w-1/2"
                 freeSolo
                 size="small"
-                disablePortal
                 options={[]}
-                renderInput={(params) => <TextField {...params} className="text-white" label={"Search Movies..."} />}
+                renderInput={(params) => <TextField {...params} label={"Search Movies..."} />}
             />
         </div>
     );
