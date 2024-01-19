c3e603c0e08acbe656e7e524f356bc4143ccda8f
Adrian <adrianhenry2115@gmail.com>
2024-01-19 16:02:30 -0500

    z index

--- components/audio-player/index.tsx
+++ components/audio-player/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AudioPlayer = () => {
     return (
-        <div className="bg-purple-300 text-white fixed bottom-0 w-full h-[75px] flex items-center">
+        <div className="bg-purple-300 z-50 text-white fixed bottom-0 w-full h-[75px] flex items-center">
             {/* ARTWORK, ARTIST, SONG, LIKE BUTTON */}
             {/* AUDIO ACTIONS AND DURATION */}
             {/* VOLUME */}
