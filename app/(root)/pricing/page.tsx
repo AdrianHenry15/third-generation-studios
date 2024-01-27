7b194066d4bcd5946f83c4f53537451c7ee289e1
Adrian <adrianhenry2115@gmail.com>
2024-01-27 14:36:10 -0500

    pricing page

---
+++ app/(root)/pricing/page.tsx
@@ -0,0 +1,12 @@
+import MusicSplash from "@/components/layout/music/music-splash";
+import Row from "@/components/row";
+import { Artists, SearchOriginalProjects } from "@/lib/projects";
+import { Category } from "@/lib/types";
+
+export default function PricingPage() {
+    return (
+        <div className="px-10 flex flex-col h-full bg-black">
+            <span>Pricing Page</span>
+        </div>
+    );
+}
