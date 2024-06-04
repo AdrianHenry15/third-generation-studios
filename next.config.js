61c2cc2c3d6c47c07a6b50b31d159f3d67c5d95a
Adrian <adrianhenry2115@gmail.com>
2024-06-04 15:46:40 -0400

    updates to env

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
     swcMinify: true,
     env: {
         DATABASE_URL: process.env.DATABASE_URL,
+        NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+        NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY,
+        NEXT_PRIVATE_KEY: process.env.NEXT_PRIVATE_KEY
     },
     images: {
         remotePatterns: [
