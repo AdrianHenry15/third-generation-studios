cf1db0caf3c7a748d6df4506ae4b79c747ee9289
AdrianHenry15 <adrianhenry2115@gmail.com>
2025-09-09 11:39:24 -0400

    update to plans and pricing

--- lib/features.tsx
+++ lib/features.tsx
@@ -22,32 +22,32 @@ import { TbMoneybag } from "react-icons/tb";
 
 export const StudioBasicFeatures = [
     {
-        icon: <MdMobileFriendly  />,
+        icon: <MdMobileFriendly />,
         feature: "Responsive Design",
         description: "Your site will look great on all devices, including desktop, tablet, and mobile",
     },
     {
-        icon: <FaSearchengin  />,
+        icon: <FaSearchengin />,
         feature: "Basic SEO Optimization",
         description: "Improve visibility on search engines with foundational SEO practices",
     },
     {
-        icon: <FaDownload  />,
+        icon: <FaDownload />,
         feature: "Standard Performance Optimization",
         description: "Faster load times to enhance user experience and retention",
     },
     {
-        icon: <GrHost  />,
+        icon: <GrHost />,
         feature: "One Year Hosting Support",
         description: "Reliable hosting and basic support for a full year",
     },
     {
-        icon: <RiPagesLine  />,
+        icon: <RiPagesLine />,
         feature: "Up to 3 pages",
         description: "Essential pages like Home, About, and Services",
     },
     {
-        icon: <IoShareSocial  />,
+        icon: <IoShareSocial />,
         feature: "Social Media Integration",
         description: "Seamless integration with social media platforms like Facebook, Instagram, and Twitter",
     },
@@ -55,32 +55,27 @@ export const StudioBasicFeatures = [
 
 export const StudioPlusFeatures = [
     {
-        icon: <FaRecycle  />,
-        feature: "All Studio Basic Features",
-        description: "Includes everything in the Studio Basic plan for a strong foundation",
-    },
-    {
-        icon: <FaSearchengin  />,
+        icon: <FaSearchengin />,
         feature: "Advanced SEO Optimization",
         description: "Enhanced SEO to improve search engine ranking and drive more organic traffic",
     },
     {
-        icon: <IoSpeedometerOutline  />,
+        icon: <IoSpeedometerOutline />,
         feature: "Performance & Speed Boost",
         description: "Advanced optimization for faster page loading and improved user experience",
     },
     {
-        icon: <MdFormatIndentIncrease  />,
+        icon: <MdFormatIndentIncrease />,
         feature: "Customizable Forms",
         description: "Custom forms for lead generation, feedback, or subscriptions, tailored to your business needs",
     },
     {
-        icon: <RiPagesLine  />,
+        icon: <RiPagesLine />,
         feature: "Up to 5 Pages",
         description: "Assistance with updating content, images, or minor changes monthly",
     },
     {
-        icon: <FcSupport  />,
+        icon: <FcSupport />,
         feature: "Monthly Content Update Support",
         description: "Expand your website with additional pages for services, testimonials, or portfolios",
     },
@@ -88,47 +83,42 @@ export const StudioPlusFeatures = [
 
 export const StudioProFeatures = [
     {
-        icon: <FaRecycle  />,
-        feature: "All Studio Plus Features",
-        description: "Includes everything in Studio Basic and Studio Plus for a comprehensive setup",
-    },
-    {
-        icon: <IoIosLogIn  />,
+        icon: <IoIosLogIn />,
         feature: "User Authentication",
         description: "Secure login and account management, ideal for member-exclusive content or user profiles",
     },
     {
-        icon: <MdOutlineEmail  />,
+        icon: <MdOutlineEmail />,
         feature: "Advanced Email Marketing & Automation",
         description: "Integrated email automation for customer onboarding, retention, and engagement campaigns",
     },
     {
-        icon: <CiMoneyBill  />,
+        icon: <CiMoneyBill />,
         feature: "E-commerce Ready",
         description: "Support for product listings, inventory management, and an integrated shopping cart (excludes payment processing)",
     },
     {
-        icon: <RiPagesLine  />,
+        icon: <RiPagesLine />,
         feature: "Up to 10 Pages",
         description: "More pages for expanded offerings, such as product catalogs, case studies, or landing pages",
     },
     {
-        icon: <MdOutlineSecurity  />,
+        icon: <MdOutlineSecurity />,
         feature: "Enhanced Security Features",
         description: "SSL encryption, firewall integration, and regular security audits for data protection",
     },
     {
-        icon: <GrHostMaintenance  />,
+        icon: <GrHostMaintenance />,
         feature: "Priority Support & Maintenance",
         description: "Fast-track support with priority for troubleshooting, monthly updates, and maintenance",
     },
     {
-        icon: <FaSearchengin  />,
+        icon: <FaSearchengin />,
         feature: "Premium SEO & Content Strategy Consultation",
         description: " In-depth SEO setup and ongoing strategy recommendations to boost visibility and engagement",
     },
     {
-        icon: <IoSpeedometerOutline  />,
+        icon: <IoSpeedometerOutline />,
         feature: "Advanced Performance Optimization",
         description: "Top-tier performance enhancements, including caching, image optimization, and code minification for maximum speed",
     },
@@ -136,53 +126,48 @@ export const StudioProFeatures = [
 
 export const StudioCommerceFeatures = [
     {
-        icon: <FaRecycle  />,
-        feature: "All Studio Pro Features",
-        description: "Includes everything in Studio Basic, Studio Plus, and Studio Pro, plus enhanced e-commerce capabilities",
-    },
-    {
-        icon: <MdPayments  />,
+        icon: <MdPayments />,
         feature: "Integrated Payment Processing (Square)",
         description:
             "Secure payment processing with Square, including setup and support for credit card, digital wallet, and local payment options",
     },
     {
-        icon: <MdAttachMoney  />,
+        icon: <MdAttachMoney />,
         feature: "Full E-Commerce Functionality",
         description: "Product catalog, shopping cart, checkout process, and order management for seamless online sales",
     },
     {
-        icon: <MdOutlineInventory2  />,
+        icon: <MdOutlineInventory2 />,
         feature: "Inventory & Stock Management",
         description: "Real-time inventory tracking and management tools to monitor product levels and avoid stockouts",
     },
     {
-        icon: <AiOutlineShoppingCart  />,
+        icon: <AiOutlineShoppingCart />,
         feature: "Abandoned Cart Recovery",
         description: "Automated email reminders for customers who leave items in their cart, helping to boost conversions",
     },
     {
-        icon: <FaRegUserCircle  />,
+        icon: <FaRegUserCircle />,
         feature: "Customer Accounts & Order History",
         description: "Customer login area with access to order history, wish lists, and saved items",
     },
     {
-        icon: <MdFilterListAlt  />,
+        icon: <MdFilterListAlt />,
         feature: "Advanced Product Filtering and Search",
         description: "Enhanced search and filtering options to help customers find products by categories, sizes, colors, etc",
     },
     {
-        icon: <GrMoney  />,
+        icon: <GrMoney />,
         feature: "Sales Tax and Shipping Calculations",
         description: "Real-time sales tax calculation and customizable shipping options to streamline the checkout experience",
     },
     {
-        icon: <TbMoneybag  />,
+        icon: <TbMoneybag />,
         feature: "Promotions & Discount Management",
         description: "Create discount codes, seasonal sales, and special promotions directly from the website",
     },
     {
-        icon: <MdAccountBalance  />,
+        icon: <MdAccountBalance />,
         feature: "Priority Support with Dedicated Account Manager",
         description: "Access to priority support with a dedicated account manager for ongoing assistance and strategy",
     },
